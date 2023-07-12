0634b074efad841526fa3ee8153cacd2f3212021
Michael Gastello <michaelgastello@aol.com>
2023-07-12 14:01:30 -0400

    remove file from git

--- src/scripts/api.js
+++
@@ -1,6 +0,0 @@
-const options = {method: 'GET', headers: {accept: 'application/json'}};
-
-export const rates = fetch('https://api.fastforex.io/fetch-all?api_key=da9d2887c7-fdabd56363-rxp2oj', options)
-  .then(response => response.json())
-  .then(response => response)
-  .catch(err => console.error(err));
